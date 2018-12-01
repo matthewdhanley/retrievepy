9c8cf93164558e51a0777010d0bc0bbfb28cf62d
Matthew Hanley <mattdhanley@gmail.com>
2018-12-01 10:12:09 -0700

    slight refactor

--- retrievepy/pylogger.py
+++ retrievepy/pylogger.py
@@ -4,6 +4,7 @@ import os
 
 CONFIG_FILE = 'logger.cfg'
 
+
 def get_logger():
     """
     Create a logging object for easy logging
@@ -12,11 +13,11 @@ def get_logger():
     # set up LOGGER from config file
     if os.path.isfile(CONFIG_FILE):
         logging.config.fileConfig(CONFIG_FILE)
-        logger = logging.getLogger('cube_ds')
+        logger = logging.getLogger('retrievepy')
     else:
         # use defaults if no config file
         format = '%(levelname)s - %(asctime)s - %(filename)s - %(funcName)s - %(lineno)d - %(message)s'
         logging.basicConfig(format=format)
-        logger = logging.getLogger('cube_ds')
+        logger = logging.getLogger('retrievepy')
         logger.warning(CONFIG_FILE+' not found. Using defaults for logging.')
     return logger
